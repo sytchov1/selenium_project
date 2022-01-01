51f08a06a78c3cfa5a7cf5baec76267f20dcbe81
sytchov1 <sytchov1@gmail.com>
2022-01-01 15:04:19 +0300

    create base structure

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en', help='Choose language (for example: fr)')
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    print("\nstart chrome browser for test..")
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit chrome browser..")
+    browser.quit()
