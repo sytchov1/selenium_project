0282b59e306141705c6e25190479970cb91f1f8f
sytchov1 <sytchov1@gmail.com>
2022-01-01 16:40:56 +0300

    create base page

---
+++ pages/base_page.py
@@ -0,0 +1,10 @@
+from _typeshed import Self
+
+
+class BasePage():
+    def __init__(self, browser, url) -> None:
+        self.browser = browser
+        self.url=url
+
+    def open(self):
+        self.browser.get(self.url)
